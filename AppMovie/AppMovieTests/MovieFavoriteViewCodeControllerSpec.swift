5385d4cf16a671994d68e518048143a2065ec40b
Ely A Ndiaye <ely.assumpcao.ndiaye@accenture.com>
2019-09-10 10:39:56 -0300

    Teste para validacao de layout da view FavoriteMovies

--- AppMovie/AppMovieTests/MovieFavoriteViewCodeControllerSpec.swift
+++ AppMovie/AppMovieTests/MovieFavoriteViewCodeControllerSpec.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2019 ely.assumpcao.ndiaye. All rights reserved.
 //
 
-import Foundation
+import Quick
+import Nimble
+import Nimble_Snapshots
+@testable import AppMovie
+
+
+class MovieFavoriteViewCodeControllerSpec: QuickSpec {
+    override func spec() {
+        describe("Screen FavoriteMovieView") {
+            it("has valid snapshot") {
+                let frame = UIScreen.main.bounds
+                let view = FavoriteMoviesView(frame: frame)
+                expect(view) == recordSnapshot("FavoriteMovieViewController")
+            }
+        }
+        
+        describe("Favorite Cell") {
+            it("Should look like this"){
+                let cell = FavoriteMoviesTableCell()
+                cell.frame = CGRect(x: 0, y: 0, width: 375, height: 190)
+                let view =  cell
+                
+                expect(view) == recordSnapshot("FavoriteCell")
+            }
+        }
+    }
+}
