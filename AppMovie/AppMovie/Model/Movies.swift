c253d2f6f019d1175e8f59bb2236227e9ac45a02
Ely A Ndiaye <ely.assumpcao.ndiaye@accenture.com>
2019-09-11 15:00:16 -0300

    Remove Controller MovieSearchViewController e Initializers descenessarios na Model Movies

--- AppMovie/AppMovie/Model/Movies.swift
+++ AppMovie/AppMovie/Model/Movies.swift
@@ -27,30 +27,7 @@ struct Result: Codable {
     var isFavorite: Bool? = false
     
     
-    //MARK: Initializers
-    public init(vote_count: Int, id: Int, title: String, video: Bool, vote_average: Double, popularity: Double, genres: [Int], overview: String, releaseYear: String, poster_path: String, original_language: String, original_title: String, backdrop_path: String, adult: Bool ) {
-        
-        self.id = Int32(id)
-        self.title = title
-        self.genre_ids = genres
-        self.overview = overview
-        self.release_date = releaseYear
-        self.poster_path = poster_path
-        self.original_language = original_language
-        self.isFavorite = false
-        
-        }
-    
-    public init(_ movieCoreData: MovieEntity) {
-        id = 1
-        title = movieCoreData.movieTitle
-        genre_ids = [0]
-        overview = movieCoreData.movieDescription
-        release_date = movieCoreData.movieDate
-        poster_path = movieCoreData.movieImage
-        original_language = "pt_BR"
-        isFavorite = movieCoreData.movieIsFavorite
-    }
+  
     
     //MARK: - Mutating Methods
     mutating func setIsFavorite() {
